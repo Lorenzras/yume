295b32cd8309360ba72c47b2d4e48f689de08927
Lorenz Ras <lenzras@gmail.com>
2021-03-26 10:59:44 +0900

    Installed dependecies
    Installed dependencies.

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,12 +1,12 @@
 <?php return array (
   'root' => 
   array (
-    'pretty_version' => '1.0.0+no-version-set',
-    'version' => '1.0.0.0',
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
     'aliases' => 
     array (
     ),
-    'reference' => NULL,
+    'reference' => '2428d3e7246bd46abd4c5cdc10f734a4350f3968',
     'name' => 'laravel/laravel',
   ),
   'versions' => 
@@ -449,12 +449,12 @@
     ),
     'laravel/laravel' => 
     array (
-      'pretty_version' => '1.0.0+no-version-set',
-      'version' => '1.0.0.0',
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
       'aliases' => 
       array (
       ),
-      'reference' => NULL,
+      'reference' => '2428d3e7246bd46abd4c5cdc10f734a4350f3968',
     ),
     'laravel/sail' => 
     array (
