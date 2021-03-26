90d96fabfd0107a8ac4a08eb9d2aaa1fa6e05094
Lorenz Ras <lenzras@gmail.com>
2021-03-26 12:16:31 +0900

    Updated database limitation
    Updated database limitation so it will compatible with server.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,7 +51,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    アプリ詳細ｇｙｈｄｄｆｓｆｓｆｓｄｆｓｄｆｓｆｓｆｓｆｓｆｓｄｆｓｄｆｓｄｆｄｆ
+                                    アプリ詳細
                                 </div>
                             </div>
                         </div>
