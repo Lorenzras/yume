b45e677a0adcdb37b05c4109981e1e5b172e28ec
Lorenz Ras <lenzras@gmail.com>
2021-03-29 06:46:31 +0900

    Added date today

--- resources/views/nippo.blade.php
+++ resources/views/nippo.blade.php
@@ -19,7 +19,7 @@
         <div class="input-group-prepend ">
             <span class="input-group-text" id="">{{ __('Date') }}</span>
         </div>
-        <input type="date" class="form-control" value="" id="date_today" required>
+        <input type="date" data-date-format="YYYY-MM-DD" class="form-control" value="" id="date_today" required>
     </div>
 
     <div class="col-12"><legend >Numbers</legend></div>
@@ -31,7 +31,7 @@
         <div class="input-group-prepend numbers">
             <span class="input-group-text" id="">{{ __('Interviews') }}</span>
         </div>
-        <input type="number" class="form-control" placeholder="0" pattern="[0-9]*" id="mendansu" required>
+        <input type="number" class="form-control" placeholder="0" pattern="[0-9]*" id="interview" required>
     </div>
 
     <div class=" form-group input-group col-sm-6">
@@ -163,21 +163,37 @@
         </div>
         <textarea class="form-control" rows="5" id="comment" required></textarea>
     </div>
-    <div class="form-group input-group col-12">
+    <div class="form-group input-group col-12 mb-5">
         <div class="input-group-prepend">
             <span class="input-group-text align-middle upright" id="">{{ __('Learned') }}</span>
         </div>
         <textarea class="form-control" rows="5" id="comment" required></textarea>
     </div>
 
+    <div class="fixed-bottom">
 
-    <div class="col-12">
-        <button type="submit" class="btn btn-lg btn-primary btn-block">{{ __('Save') }}</button>
+        <div class="col-12">
+            <button type="submit" class="btn btn-lg btn-primary btn-block">{{ __('Save') }}</button>
+        </div>
     </div>
+
 
     </form>
+
+
     </div>
 
+    <script>
+        window.onload = function() {
+        var today = new Date();
+        var month = (today.getMonth()+1);
+        if ((month+'').length == 1) {
+            month = "0" + month;
+        }
+        var date = today.getFullYear()+'-' +month+'-'+today.getDate();
+        document.getElementById("date_today").value = date;
 
+        }
+    </script>
 
 @endsection
