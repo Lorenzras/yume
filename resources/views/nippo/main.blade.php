cd345d69e8407b15e53a8a5dd5fedbdc04281ec2
Lorenz Ras <lenzras@gmail.com>
2021-04-07 12:18:36 +0900

    hide menu on scroll

--- resources/views/nippo/main.blade.php
+++ resources/views/nippo/main.blade.php
@@ -197,13 +197,23 @@
 window.onload = function() {
     $(document).click(function (event) {
         console.log('clicked DOM');
-    var clickover = $(event.target);
-    var $navbar = $(".navbar-collapse");
-    var _opened = $navbar.hasClass("in");
-    if (_opened === true && !clickover.hasClass("navbar-toggle")) {
-        $navbar.collapse('hide');
-    }
+        var clickover = $(event.target);
+        var $navbar = $(".navbar-collapse");
+        var _opened = $navbar.hasClass("in");
+        if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+            $navbar.collapse('hide');
+        }
     });
+
+    $(window).scroll(function() {
+        console.log('logging');
+        var $navbar = $(".navbar-collapse");
+        var _opened = $navbar.hasClass("in");
+        if (_opened === true) {
+            $navbar.collapse('hide');
+        }
+    });
+
 
 };
 </script>
