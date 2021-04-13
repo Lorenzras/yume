c5ff0d7fee8b1fa1e8abd85743d3e7877df314c0
Lorenz Ras <lenzras@gmail.com>
2021-04-13 13:32:28 +0900

    frontend initialize

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1,  shrink-to-fit=no">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -97,9 +97,9 @@
             </div>
         </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+
+        @yield('content')
+ 
     </div>
 
     <script type="application/javascript" defer>
