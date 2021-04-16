1993e8b01edc48347d7f7ac310dd27859a0f988c
Lorenz Ras <lenzras@gmail.com>
2021-04-16 14:30:02 +0900

    Update chatworkbot.blade.php

--- resources/views/chatworkbot.blade.php
+++ resources/views/chatworkbot.blade.php
@@ -1,9 +1,26 @@
 <?php
 
+
 $raw = file_get_contents('php://input');
 $receive = json_decode($raw, true);
-Log::info($receive);
-//error_log(print_r($receive,true),"3","/var/log/php.log");
+
+if ($receive['webhook_event']['account_id'] == '1qh9jubt8pcqi' ) exit();
+
+$message = $receive['webhook_event']['body'];
+
+$reply = array('body' => $message );
+
+header('Content-type: application/json; charset=utf-8');
+
+$cdata = curl_init();
+curl_setopt($cdata, CURLOPT_URL, 'https://api.chatwork.com/v2/rooms/225800073/messages');
+curl_setopt($cdata, CURLOPT_HTTPHEADER, array('X-ChatWorkToken: 2AzlszjpQLVdP39Qm23aL0vHh+j28C8P71FS0lISCQI='));
+curl_setopt($cdata, CURLOPT_POSTFIELDS, http_build_query($reply, '', '&'));
+curl_setopt($cdata, CURLOPT_POST, 1);
+curl_setopt($cdata, CURLOPT_RETURNTRANSFER, 1);
+
+$ret = curl_exec($cdata);
+curl_close($data);
 
 ?>
 
