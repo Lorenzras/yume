5e8afb019e61923d8883e9da042b3f667aaef98a
Lorenz Ras <lenzras@gmail.com>
2021-04-16 14:33:15 +0900

    Update chatworkbot.blade.php

--- resources/views/chatworkbot.blade.php
+++ resources/views/chatworkbot.blade.php
@@ -14,7 +14,7 @@ header('Content-type: application/json; charset=utf-8');
 
 $cdata = curl_init();
 curl_setopt($cdata, CURLOPT_URL, 'https://api.chatwork.com/v2/rooms/225800073/messages');
-curl_setopt($cdata, CURLOPT_HTTPHEADER, array('X-ChatWorkToken: 2AzlszjpQLVdP39Qm23aL0vHh+j28C8P71FS0lISCQI='));
+curl_setopt($cdata, CURLOPT_HTTPHEADER, array('X-ChatWorkToken: 7bc795ef967064f642aa70956cde3cad'));
 curl_setopt($cdata, CURLOPT_POSTFIELDS, http_build_query($reply, '', '&'));
 curl_setopt($cdata, CURLOPT_POST, 1);
 curl_setopt($cdata, CURLOPT_RETURNTRANSFER, 1);
